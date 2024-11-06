f1cc433504c41bff24c4b09b8ccdfb176c532765
NilsRo <51311453+NilsRo@users.noreply.github.com>
2024-11-06 16:23:00 +0100

    0.2.0
    * add options for checking backup destination if it's a mount point, for people that want to use NAS destinations and symlink to backup folder on the device running OctoPrint. Helpful in reducing writes to SD card. 
    * add email notification options related to new mount checking, backup failure, and backup success.
    * limit to OctoPrint versions 1.9.0+

--- octoprint_backupscheduler/static/js/backupscheduler.js
+++ octoprint_backupscheduler/static/js/backupscheduler.js
@@ -4,33 +4,106 @@
  * Author: jneilliii
  * License: MIT
  */
-$(function() {
+$(function () {
     function BackupschedulerViewModel(parameters) {
         var self = this;
 
         self.settingsViewModel = parameters[0];
-        self.cert_saved = ko.observable(false);
-        self.show_time = ko.pureComputed(function(){
-            return self.settingsViewModel.settings.plugins.backupscheduler.backup_daily() || self.settingsViewModel.settings.plugins.backupscheduler.backup_weekly() || self.settingsViewModel.settings.plugins.backupscheduler.backup_monthly()
-        })
+        self.sendTestEmailRunning = ko.observable(false);
 
-        self.onSettingsBeforeSave = function(){
-            if(!self.settingsViewModel.settings.plugins.backupscheduler.daily.time().match(/([01]?[0-9]|2[0-3]):[0-5][0-9]/)){
-                self.settingsViewModel.settings.plugins.backupscheduler.daily.time('00:00')
+        // Hack to remove automatically added Cancel button
+		// See https://github.com/sciactive/pnotify/issues/141
+		PNotify.prototype.options.confirm.buttons = [];
+
+        //send retained notification
+        self.onStartupComplete = function () {
+            if (self.settingsViewModel.settings.plugins.backupscheduler.notification.retained_message !== "") {
+                var payload = ko.toJS(self.settingsViewModel.settings.plugins.backupscheduler.notification.retained_message);
+                self.onDataUpdaterPluginMessage("backupscheduler", payload);
             }
-            if(!self.settingsViewModel.settings.plugins.backupscheduler.weekly.time().match(/([01]?[0-9]|2[0-3]):[0-5][0-9]/)){
-                self.settingsViewModel.settings.plugins.backupscheduler.weekly.time('00:00')
+        };
+
+        self.onSettingsBeforeSave = function () {
+            if (!self.settingsViewModel.settings.plugins.backupscheduler.daily.time().match(/([01]?[0-9]|2[0-3]):[0-5][0-9]/)) {
+                self.settingsViewModel.settings.plugins.backupscheduler.daily.time('00:00');
             }
-            if(!self.settingsViewModel.settings.plugins.backupscheduler.monthly.time().match(/([01]?[0-9]|2[0-3]):[0-5][0-9]/)){
-                self.settingsViewModel.settings.plugins.backupscheduler.monthly.time('00:00')
+            if (!self.settingsViewModel.settings.plugins.backupscheduler.weekly.time().match(/([01]?[0-9]|2[0-3]):[0-5][0-9]/)) {
+                self.settingsViewModel.settings.plugins.backupscheduler.weekly.time('00:00');
             }
-        }
+            if (!self.settingsViewModel.settings.plugins.backupscheduler.monthly.time().match(/([01]?[0-9]|2[0-3]):[0-5][0-9]/)) {
+                self.settingsViewModel.settings.plugins.backupscheduler.monthly.time('00:00');
+            }
+        };
+
+        // receive data from server
+        self.onDataUpdaterPluginMessage = function (plugin, data) {
+            // exit early if not from this plugin
+            if (plugin !== "backupscheduler") {
+                return;
+            }
+
+            // NotificationMessages
+
+            if (data.notifyType) {
+                var notfiyType = data.notifyType;
+                var notifyMessage = "\n" + data.notifyTitle + ":\n" + data.notifyMessage;
+                var notifyHide = data.notifyHide;
+                self.notification_popup = new PNotify({
+                    title: "Backup Scheduler",
+                    text: notifyMessage,
+                    type: notfiyType,
+                    hide: notifyHide,
+                    confirm: {
+                        confirm: true,
+                        buttons: [{
+                            text: gettext('Clear Error'),
+                            addClass: 'btn-danger',
+                            promptTrigger: true,
+                            click: function(notice, value){
+                                notice.remove();
+                                notice.get().trigger("pnotify.cancel", [notice, value]);
+                            }
+                        }]
+                    },
+                    buttons: {
+                        closer: false,
+                        sticker: false,
+                    },
+                    history: {
+                        history: false
+                    }
+                });
+                self.notification_popup.get().on('pnotify.cancel', function() {self.resetRetainedNotifyMessageID();});
+            }
+
+            if(data.clear_notification && typeof self.notification_popup !== "undefined") {
+                self.notification_popup.remove();
+                self.notification_popup = undefined;
+            }
+        };
+
+        // Send an Email to test settings
+        self.sendTestEmail = function () {
+            self.sendTestEmailRunning(true);
+            OctoPrint.simpleApiCommand("backupscheduler", "sendTestEmail", {}).done(function (data) {
+                console.log(data);
+                self.sendTestEmailRunning(false);
+            }).fail(function (data) {
+                self.sendTestEmailRunning(false);
+            });
+        };
+
+        self.resetRetainedNotifyMessageID = function () {
+            OctoPrint.simpleApiCommand("backupscheduler", "clearRetainedMessage", {}).done(function (data) {
+                console.log(data);
+            });
+        };
     }
 
     OCTOPRINT_VIEWMODELS.push({
         construct: BackupschedulerViewModel,
         // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
-        dependencies: [ "settingsViewModel" ],
-        elements: [ "#settings_plugin_backupscheduler" ]
+        dependencies: ["settingsViewModel"],
+        elements: ["#settings_plugin_backupscheduler"]
     });
 });
