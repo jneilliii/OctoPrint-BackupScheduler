5413640324960a026946614e1faccd2e73bb91c2
jneilliii <jneilliii+github@gmail.com>
2024-11-09 00:39:55 -0500

    0.2.1 pin cryptography dependency for better compatibility, #27

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_backupscheduler"
 plugin_name = "Backup Scheduler"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.2.0"
+plugin_version = "0.2.1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/jneilliii/OctoPrint-BackupScheduler"
 plugin_license = "MIT"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["cryptography"]
+plugin_requires = ["cryptography>=38.0.0,<40"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
